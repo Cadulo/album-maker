331a285370b06a11ba0de560a835fa6cf8fa1385
Carlos Zambrano <cmzambra@espol.edu.ec>
2023-07-09 12:51:51 -0500

    Adminstrator order + panel

--- src/pages/OrderList.js
+++ src/pages/OrderList.js
@@ -1,80 +1,91 @@
-
 import { useEffect, useState } from "react";
 import Navbar from "../components/navbar";
 import { useOrder } from "../context/OrderContext";
 import { Order } from "../components/Order";
 
-
 export const OrderList = () => {
-  const { getOrder } = useOrder();
-  const [isLoading, setIsLoading] = useState(false);
-  const [listFiles, setListFiles] = useState([]);
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setIsLoading(true);
-        const res = await getOrder();
-        setListFiles(res);
-        console.log(res);
-        setIsLoading(false);
-      } catch (error) {
-        console.error("Error ", error);
-      }
-    };
-    fetchData();
-  }, []);
+    const { getOrder } = useOrder();
+    const [isLoading, setIsLoading] = useState(false);
+    const [listFiles, setListFiles] = useState([]);
 
-  return (
-    <div className="container mx-auto dark:text-white  dark:bg-slate-900">
-      <Navbar></Navbar>
-      {listFiles.length !== 0 ? (
-        <div className="text-center text-2xl font-bold my-8">Mis órdenes:</div>
-      ) : (
-        <div className="text-center text-2xl font-bold my-8">
-          Aún no tienes órdenes
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setIsLoading(true);
+                const res = await getOrder();
+                setListFiles(res);
+                console.log(res);
+                setIsLoading(false);
+            } catch (error) {
+                console.error("Error ", error);
+            }
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div className="container mx-auto dark:text-white  dark:bg-slate-900">
+            <Navbar></Navbar>
+            {listFiles.length !== 0 ? (
+                <div className="text-center text-2xl font-bold my-8">
+                    Mis órdenes:
+                </div>
+            ) : (
+                <div className="text-center text-2xl font-bold my-8">
+                    Aún no tienes órdenes
+                </div>
+            )}
+            {isLoading ? (
+                <div>
+                    <div className="text-center text-2xl font-bold my-8">
+                        Cargando...
+                    </div>
+                </div>
+            ) : (
+                listFiles &&
+                listFiles.length > 0 && (
+                    <div className="flex flex-column justify-center mt-4 mx-4 border-gray-800">
+                        <div className="">
+                            {listFiles.map((file, index) => {
+                                return (
+                                    <div
+                                        key={index}
+                                        className="text-black border-2 my-11">
+                                        <table className="min-w-full bg-white border mb-2">
+                                            <thead>
+                                                <tr className="my-8">
+                                                    <th className="py-2 px-4 border-b ">
+                                                        Estado
+                                                    </th>
+                                                    <th className="py-2 px-4 border-b ">
+                                                        Courier
+                                                    </th>
+                                                </tr>
+                                            </thead>
+                                            <tbody className="text-center">
+                                                <tr>
+                                                    <td className="py-2 px-4 border-b ">
+                                                        {file.status}
+                                                    </td>
+                                                    <td className="py-2 px-4 border-b ">
+                                                        {file.courier}
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                        <Order
+                                            billId={file.bill}
+                                            shippingId={file.shipping}
+                                            orderId={file._id}
+                                            key={index}
+                                            showBotton={true}></Order>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                )
+            )}
         </div>
-      )}
-      {isLoading ? (
-        <div>
-          <div className="text-center text-2xl font-bold my-8">Cargando...</div>
-        </div>
-      ) : (
-        listFiles &&
-        listFiles.length > 0 && (
-          <div className="flex flex-column justify-center mt-4 mx-4 ">
-            <div className="">
-              {listFiles.map((file, index) => {
-                return (
-                  <div key={index} className=" text-black">
-                    <table className="min-w-full bg-white border border-gray-300 mb-4 ">
-                      <thead>
-                        <tr>
-                          <th className="py-2 px-4 border-b ">Estado</th>
-                          <th className="py-2 px-4 border-b ">Courier</th>              
-                        </tr>
-                      </thead>
-                      <tbody className="text-center">
-                        <tr>
-                          <td className="py-2 px-4 border-b ">{file.status}</td>
-                          <td className="py-2 px-4 border-b ">{file.courier}</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                    <Order
-                      billId={file.bill}
-                      shippingId={file.shipping}
-                      orderId={file._id}
-                      key={index}
-                      showBotton={true}
-                    ></Order>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        )
-      )}
-    </div>
-  );
+    );
 };
--- src/pages/PanelOrders.js
+++ src/pages/PanelOrders.js
@@ -5,84 +5,93 @@ import { Order } from "../components/Order";
 import { useAuth } from "../context/AuthContext";
 
 export const PanelOrders = () => {
-  const { user } = useAuth();
-  const { getOrderAdmin} = useOrder();
-  const [isLoading, setIsLoading] = useState(false);
-  const [listFiles, setListFiles] = useState([]);
-  const [showBotton, setShowBotton] = useState(false)
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setIsLoading(true);
-        const res = await getOrderAdmin();
-        console.log(res)
-        setListFiles(res);
-        setIsLoading(false);
-      } catch (error) {
-        console.error("Error ", error);
-      }
-    };
-    fetchData();
-  }, []);
-  if (user.isAdmin || user.username === "admin") {
-    return (
-      <div className="container mx-auto dark:text-white  dark:bg-slate-900">
-        <Navbar></Navbar>
-        {listFiles &&
-          (listFiles.length !== 0 ? (
-            <div className="text-center text-2xl font-bold my-8">
-              Administra:
-            </div>
-          ) : (
-            <div className="text-center text-2xl font-bold my-8">
-              No hay nada que administrar aún
-            </div>
-          ))}
-        {isLoading ? (
-          <div>
-            <div className="text-center text-2xl font-bold my-8">
-              Cargando...
-            </div>
-          </div>
-        ) : (
-          listFiles &&
-          listFiles.length > 0 && (
-            <div className="flex flex-column justify-center mt-4 mx-4">
-              <div className="">
-                {listFiles.map((file, index) => {
-                  return (
-                    <div key={index} className="text-black">
-                    <table className="min-w-full bg-white border border-gray-300 mb-4 dark:text-dark">
-                      <thead>
-                        <tr>
-                          <th className="py-2 px-4 border-b">Estado</th>
-                          <th className="py-2 px-4 border-b">Courier</th>              
-                        </tr>
-                      </thead>
-                      <tbody className="text-center">
-                        <tr>
-                          <td className="py-2 px-4 border-b">{file.status}</td>
-                          <td className="py-2 px-4 border-b">{file.courier}</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                    <Order
-                      billId={file.bill}
-                      shippingId={file.shipping}
-                      orderId={file._id}
-                      key={index}
-                      showBotton={showBotton}
-                    ></Order>
+    const { user } = useAuth();
+    const { getOrderAdmin } = useOrder();
+    const [isLoading, setIsLoading] = useState(false);
+    const [listFiles, setListFiles] = useState([]);
+    const [showBotton, setShowBotton] = useState(false);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setIsLoading(true);
+                const res = await getOrderAdmin();
+                console.log(res);
+                setListFiles(res);
+                setIsLoading(false);
+            } catch (error) {
+                console.error("Error ", error);
+            }
+        };
+        fetchData();
+    }, []);
+    if (user.isAdmin || user.username === "admin") {
+        return (
+            <div className="container mx-auto dark:text-white  dark:bg-slate-900">
+                <Navbar></Navbar>
+                {listFiles &&
+                    (listFiles.length !== 0 ? (
+                        <div className="text-center text-2xl font-bold my-8">
+                            Administra:
+                        </div>
+                    ) : (
+                        <div className="text-center text-2xl font-bold my-8">
+                            No hay nada que administrar aún
+                        </div>
+                    ))}
+                {isLoading ? (
+                    <div>
+                        <div className="text-center text-2xl font-bold my-8">
+                            Cargando...
+                        </div>
                     </div>
-                  );
-                })}
-              </div>
+                ) : (
+                    listFiles &&
+                    listFiles.length > 0 && (
+                        <div className="flex flex-column justify-center mt-4 mx-4">
+                            <div className="">
+                                {listFiles.map((file, index) => {
+                                    return (
+                                        <div
+                                            key={index}
+                                            className="text-black border-2 my-11">
+                                            <table className="min-w-full bg-white border border-gray-300 mb-4 dark:text-dark">
+                                                <thead>
+                                                    <tr>
+                                                        <th className="py-2 px-4 border-b">
+                                                            Estado
+                                                        </th>
+                                                        <th className="py-2 px-4 border-b">
+                                                            Courier
+                                                        </th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody className="text-center">
+                                                    <tr>
+                                                        <td className="py-2 px-4 border-b">
+                                                            {file.status}
+                                                        </td>
+                                                        <td className="py-2 px-4 border-b">
+                                                            {file.courier}
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                            <Order
+                                                billId={file.bill}
+                                                shippingId={file.shipping}
+                                                orderId={file._id}
+                                                key={index}
+                                                showBotton={showBotton}></Order>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                    )
+                )}
             </div>
-          )
-        )}
-      </div>
-    );
-  } else {
-    return <div>No estas autorizado para entrar a este sitio</div>;
-  }
+        );
+    } else {
+        return <div>No estas autorizado para entrar a este sitio</div>;
+    }
 };
