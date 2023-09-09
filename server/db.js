eb8c40063f5dc4072dc128883b617c077931ef4f
Carlos Lopez <celopez28@outlook.com>
2023-09-09 18:50:51 -0500

    'db'

--- server/db.js
+++ server/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose")
 
-const user = 'admin'
-const password = 'admin'
+const user = 'cadulo'
+const password = 'PBLHFbkApgVVzRLA'
 const database_name ='album-maker'
-const url = `mongodb+srv://${user}:${password}@cluster0.7vfqa1k.mongodb.net/${database_name}?retryWrites=true&w=majority`
+const url = `mongodb+srv://cadulo:${password}@gallerydb.4vqdol5.mongodb.net/?retryWrites=true&w=majority`
 // const url = 'mongodb://127.0.0.1:27017/'
 
 mongoose.connect(url)
